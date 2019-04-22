2ab0acf0773cb20c1809aa7de4c6f80cf8ac1dee
ruiyanc <1316650134@qq.com>
2019-04-22 22:04:11 +0800

    test1.26

--- src/com/company/Jdbc.java
+++ src/com/company/Jdbc.java
@@ -7,15 +7,16 @@ import java.sql.SQLException;
 public class demo1 {
     public static void main(String[] args)  {
         try {
-        Connection connection = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "yanrui", "xu1998");
-        String sql = "select * from students";
-        Statement statement = null;
+            Class.forName("com.mysql.jdbc");
+            Connection connection = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "yanrui", "xu1998");
+            String sql = "select * from students";
+            Statement statement = null;
             statement = (Statement) connection.createStatement();
             boolean count = statement.execute(sql);
             System.out.println(count);
             statement.close();
             connection.close();
-        } catch (SQLException e) {
+        } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
